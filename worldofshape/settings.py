b20be2ff735e688dba178f4622ec15a237d20b7b
mrbrunotte <mrbrunotte@gmail.com>
2020-03-15 17:34:21 +0100

    Add login and logout system

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -130,4 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CRISPY_TMEPLATE_PACK = 'bootstrap4.4.1'
+CRISPY_TMEPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
