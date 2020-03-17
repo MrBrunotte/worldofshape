7e8d2fc55c80487273c66319def0c22994bc8fa9
mrbrunotte <mrbrunotte@gmail.com>
2020-03-17 11:42:33 +0100

    Add create function

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 # Create your views here.
@@ -8,8 +14,28 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'home.html', context)
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<view>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
-    return render(request, 'about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
