477b347e26622df0f82cd951dbfc3e985b1ed30a
mrbrunotte <mrbrunotte@gmail.com>
2020-03-30 21:56:53 +0200

    Uncommitted import env..test if Travis passes

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 # imports the env.py file and keys
-# import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
