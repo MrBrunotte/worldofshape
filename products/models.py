baede9cd175205d26ea2c16986f27ed4693f61f6
mrbrunotte <mrbrunotte@gmail.com>
2020-04-04 10:30:54 +0200

    add full_description field to products

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     """
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    full_description = models.TextField(default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='')
 
