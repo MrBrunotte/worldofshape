03752d6f8ff228c32208db7e548eded6e64ff4b0
mrbrunotte <mrbrunotte@gmail.com>
2020-05-15 17:34:01 +0200

    Add changes to settings file

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -158,22 +158,31 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
 
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
 }
-AWS_STORAGE_BUCKET_NAME = 'worldofshape'
-AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 # to make sure files are not overwritten if they have the same name
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3,S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
+# This will make sure that the file URL does not have unnecessary parameters like your access key.
+# This is usefull when the bucket is public like this one
+AWS_QUERYSTRING_AUTH = False
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),
