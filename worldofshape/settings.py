c3df7758df900753ecbb4dbce16812b4f61e50a7
mrbrunotte <mrbrunotte@gmail.com>
2020-05-15 14:21:28 +0200

    Add settings for db

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -117,11 +117,12 @@ else:
         }
     }
 
-# else:
-#    DATABASES = {
-#        'default': {
-#            'name': env('DATABASE_URL')}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
