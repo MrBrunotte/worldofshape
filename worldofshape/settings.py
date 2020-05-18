5d4b795f89d33c9eaac9b0e77baa6b68b3546ce9
mrbrunotte <mrbrunotte@gmail.com>
2020-05-18 14:06:38 +0200

    Revert "Revert "Add changes to settings file""
    This reverts commit 3ca09c770f8497330329063b31f783437bf764a7.
    
    Discard changes made after this commit

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -180,21 +180,6 @@ USE_TZ = True
 #STATICFILES_LOCATION = 'static'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000',
-}
-AWS_STORAGE_BUCKET_NAME = 'worldofshape'
-AWS_S3_REGION_NAME = 'eu-north-1'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# to make sure files are not overwritten if they have the same name
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
