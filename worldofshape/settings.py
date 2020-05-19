d6a72f350b6777348c94f647598a2de102245d49
mrbrunotte <mrbrunotte@gmail.com>
2020-05-19 12:37:22 +0200

    Add if statement for dbs

--- worldofshape/settings.py
+++ worldofshape/settings.py
@@ -104,24 +104,24 @@ WSGI_APPLICATION = 'worldofshape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if "DATABASE_URL" in env:
-#    DATABASES = {'default': dj_database_url.parse(
-#        os.environ.get("DATABASE_URL"))}
-# else:
-#    print("Database URL not found. Using SQLite instead")
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        }
-#    }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DATABASE_URL" in env:
+    DATABASES = {'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
